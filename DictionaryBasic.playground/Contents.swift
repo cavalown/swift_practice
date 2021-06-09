54dbcdc3e882b38c5f48411365e58ed5c5cd3279
cavalown <cavalown@gmail.com>
2021-06-09 23:13:28 +0800

    Swift dictionary introduce

--- DictionaryBasic.playground/Contents.swift
+++ DictionaryBasic.playground/Contents.swift
@@ -1,3 +1,42 @@
-import UIKit
+// - Dictionary
+var fruitDict = ["red":"apple", "yellow":"banana", "green":"mango"]
+fruitDict["green"]
+fruitDict["yellow"]
+fruitDict["brown"] // get no key
 
-var greeting = "Hello, playground"
+// - change value in a dictionary
+fruitDict["green"] = "watermelon"
+print(fruitDict)
+fruitDict["red"] = "tomato"
+print(fruitDict)
+fruitDict.updateValue("kiwi", forKey: "green")
+print(fruitDict)
+fruitDict.updateValue("cherry", forKey: "red")
+print(fruitDict)
+
+
+// - add element in a dictionary
+fruitDict["orange"] = "orange"
+print(fruitDict)
+fruitDict["purple"] = "grape"
+print(fruitDict)
+fruitDict.updateValue("blackberry", forKey: "blue")
+print(fruitDict)
+
+
+// - delete element from a dictionary
+fruitDict["red"] = nil
+print(fruitDict)
+fruitDict.removeValue(forKey: "yellow")
+print(fruitDict)
+
+
+// practice
+var score = ["english":90, "sport":80, "chinese":73]
+score["english"]
+score["chinese"] = 65
+
+// - type of elements in a dictionary
+var newDict:[String:Int] = ["A":100, "B":80, "C":60]
+let basicScore = newDict["C"]
+
