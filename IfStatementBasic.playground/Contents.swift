2c25b3cd7415dafcdbea843b78e3a23eaa46e750
cavalown <cavalown@gmail.com>
2021-06-10 00:56:04 +0800

    Swift ifStatement introduce

---
+++ IfStatementBasic.playground/Contents.swift
@@ -0,0 +1,37 @@
+var price = 80
+
+/*
+if price == 80{
+    // what to do if the price equal to 80
+    print("Bur it!")
+}
+*/
+
+if price > 150{
+    print("Are you crazy?")
+}else if price < 50{
+    print("Very cheap!")
+}else{
+    print("Buy it.")
+}
+
+let myName = "Thomas"
+let time = 15
+let isHungry = true
+
+
+if myName == "Thomas" && time < 12 && isHungry == true{
+    print("Hellom \(myName). Do you want to have some breakfast?")
+}else if myName == "Friedrich" || myName == "Thomas"{
+    print("Guten Tag, \(myName)")
+}else{
+    print("Who are you?")
+}
+
+
+var isOK = true
+isOK =  !isOK
+isOK
+
+
+
